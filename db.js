6d20878ca02ade8e66928835c6cb371e34feef85
sadmanador <sadman.ador@gmail.com>
2024-07-17 21:06:10 +0600

    dotenv and atlas connection made

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
+const constants = require("./configs/constants");
 
 const todoConnection = mongoose
-  .connect("mongodb://localhost:27017/todos")
+  .connect(constants.mongoDbConnection, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connection with db is successful");
   })
   .catch((err) => console.log(err));
 
-  
-  module.exports = {todoConnection}
+module.exports = { todoConnection };
